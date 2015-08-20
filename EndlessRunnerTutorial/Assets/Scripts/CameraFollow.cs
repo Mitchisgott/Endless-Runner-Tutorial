e6659b944d89a95868d6024d0f17907a8c230889
Mitchisgott <mitchell.gott@gmail.com>
2015-08-19 22:29:29 -0400

    Player Asset
    Added a player asset. Created a camera follow script. "started" level
    generation script. created game scene. added player to scene and added
    camera follow script to camera.

---
+++ EndlessRunnerTutorial/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public float followDistance;
+
+	private GameObject player;
+
+	void Start()
+	{
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+
+	void Update () {
+		transform.position = new Vector3 (0, 2, player.transform.position.z - followDistance);
+	}
+}
---
+++ EndlessRunnerTutorial/Assets/Scripts/LevelGeneration.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelGeneration : MonoBehaviour {
+
+	public GameObject[] obstacles;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void CreateObject(int objNum)
+	{
+		GameObject newObj = Instantiate (obstacles [objNum]);
+	}
+}
