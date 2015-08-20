fe2f26762e7a9c5ccd67c661abcca64a8a1cb04e
Mitchisgott <mitchell.gott@gmail.com>
2015-08-20 00:43:37 -0400

    GroundMovement
    Created Ground Movement Script and implemented it into the game scene.
    Added Tunnel Exit texture and plane game object into game scene.

---
+++ EndlessRunnerTutorial/Assets/Scripts/GroundMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundMovement : MonoBehaviour {
+
+	public float movementSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector3.back * movementSpeed * Time.deltaTime);
+		if(transform.position.z < -10)
+		{
+			transform.position = new Vector3(0,0,0);
+		}
+	}
+}
