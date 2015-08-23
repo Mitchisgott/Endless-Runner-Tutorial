5867849f21d219b2adcef6316fde7c7bdd6edee2
Mitchisgott <mitchell.gott@gmail.com>
2015-08-23 18:39:02 -0400

    Player Movement
    Started creating player movement script.

---
+++ EndlessRunnerTutorial/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.Translate(Vector3.left * speed * Time.deltaTime);
+		}
+		if(Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.Translate(Vector3.right * speed * Time.deltaTime);
+		}
+		if(Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			transform.GetComponent<Animation>().Play("flip");
+			transform.GetComponent<Animation>().PlayQueued("run");
+		}
+		if(Input.GetKeyDown(KeyCode.DownArrow))
+		{
+			transform.GetComponent<Animation>().Play("death");
+			transform.GetComponent<Animation>().PlayQueued("run");
+		}
+	}
+}
